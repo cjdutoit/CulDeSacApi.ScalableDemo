9032ba01e7b52cb3b525de9947ebc816dbd5798f
Christo du Toit <christo@dutoit.co.uk>
2022-06-10 20:13:27 +0100

    DATA: Student

---
+++ CulDeSacApi/Models/Students/Student.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit All rights reserved.
+// Licensed under the MIT License.
+// ---------------------------------------------------------------
+
+using System;
+
+namespace CulDeSacApi.Models.Students
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}

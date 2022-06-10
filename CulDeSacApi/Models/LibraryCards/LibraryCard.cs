86154c75218aff83cba990ab4a088978755c3cea
Christo du Toit <christo@dutoit.co.uk>
2022-06-11 00:27:14 +0100

    DATA: LibraryCard

---
+++ CulDeSacApi/Models/LibraryCards/LibraryCard.cs
@@ -0,0 +1,18 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit All rights reserved.
+// Licensed under the MIT License.
+// ---------------------------------------------------------------
+
+using System;
+using CulDeSacApi.Models.LibraryAccounts;
+
+namespace CulDeSacApi.Models.LibraryCards
+{
+    public class LibraryCard
+    {
+        public Guid Id { get; set; }
+
+        public Guid LibraryAccountId { get; set; }
+        public LibraryAccount LibraryAccount { get; set; }
+    }
+}

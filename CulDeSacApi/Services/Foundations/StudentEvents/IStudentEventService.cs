1c1756aa3743c5a0d0e6fcefc8de3477e11e1c7f
Christo du Toit <christo@dutoit.co.uk>
2022-06-10 22:40:15 +0100

    ShouldSubscribeToStudentEvent -> FAIL

---
+++ CulDeSacApi/Services/Foundations/StudentEvents/IStudentEventService.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit All rights reserved.
+// Licensed under the MIT License.
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using CulDeSacApi.Models.Students;
+
+namespace CulDeSacApi.Services.Foundations.StudentEvents
+{
+    public interface IStudentEventService
+    {
+        void SubscribeToStudentAddEvent(Func<Student, ValueTask<Student>> studentEventHandler);
+    }
+}
---
+++ CulDeSacApi/Services/Foundations/StudentEvents/StudentEventService.cs
@@ -0,0 +1,23 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit All rights reserved.
+// Licensed under the MIT License.
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using CulDeSacApi.Brokers.Events;
+using CulDeSacApi.Models.Students;
+
+namespace CulDeSacApi.Services.Foundations.StudentEvents
+{
+    public class StudentEventService : IStudentEventService
+    {
+        private readonly IEventBroker eventBroker;
+
+        public StudentEventService(IEventBroker eventBroker) =>
+            this.eventBroker = eventBroker;
+
+        public void SubscribeToStudentAddEvent(Func<Student, ValueTask<Student>> studentEventHandler) =>
+            throw new NotImplementedException();
+    }
+}

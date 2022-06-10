091167a08877f7697b8be908cd219194fa2f9220
Christo du Toit <christo@dutoit.co.uk>
2022-06-10 21:08:07 +0100

    BROKERS: Publish Student Add Event

---
+++ CulDeSacApi/Brokers/Events/EventBroker.Students.cs
@@ -0,0 +1,19 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit All rights reserved.
+// Licensed under the MIT License.
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using CulDeSacApi.Models.Students;
+
+namespace CulDeSacApi.Brokers.Events
+{
+    public partial class EventBroker : IEventBroker
+    {
+        private static Func<Student, ValueTask<Student>> StudentEventHandler;
+
+        public async ValueTask PublishStudentAddEventAsync(Student student) =>
+            await StudentEventHandler(student);
+    }
+}
---
+++ CulDeSacApi/Brokers/Events/IEventBroker.Students.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Christo du Toit All rights reserved.
+// Licensed under the MIT License.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using CulDeSacApi.Models.Students;
+
+namespace CulDeSacApi.Brokers.Events
+{
+    public partial interface IEventBroker
+    {
+        ValueTask PublishStudentAddEventAsync(Student student);
+    }
+}
